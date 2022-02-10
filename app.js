0d176b262f6b431b5868c6539cfe3adfbb9981ef
Gerhard <gerhard.putter@yahoo.com>
2022-02-10 20:28:44 +0200

    First commit.

---
+++ app.js
@@ -0,0 +1,7 @@
+const firsttext = document.querySelector("textarea[name=textarea-1]")
+const finaltext = document.querySelector("textarea[name=textarea-2]")
+const button = document.querySelector("button")
+
+button.addEventListener("click", function () {
+    console.log("click")
+})
