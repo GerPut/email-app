b3779ee053a1fd650f86355bd1bc8f42bc8cd2f7
Gerhard <gerhard.putter@yahoo.com>
2022-02-14 16:46:16 +0200

    lenght of email list.

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const firsttext = document.querySelector("textarea[name=textarea-1]")
 const finaltext = document.querySelector("textarea[name=textarea-2]")
 const button = document.querySelector("button")
+const number = document.querySelector(".number")
 
 button.addEventListener("click", function () {
     let temp = firsttext.value
     let expression = /([A-Za-z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi
     let emaildata = temp.match(expression)
-    console.log(emaildata)
+
+    finaltext.innerText = emaildata
+    number.innerHTML = emaildata.length
 })
